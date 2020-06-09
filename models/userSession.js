adf88be5c2765a32a8fbb54fc4eadb876f3aada3
Ana Medrano <amedrano.fernandez@gmail.com>
2020-06-09 10:49:05 -0700

    updated models

--- models/userSession.js
+++ models/userSession.js
@@ -1,6 +1,13 @@
 module.exports = function(sequelize, Datatypes) {
     var UserSession = sequelize.define("UserSession", {
-    })
+        session: {
+            type: Datatypes.STRING,
+            allowNull: false,
+        }
+    });
+
+    UserSession.associate = models => {
+        UserSession.belongsTo(models.User);
 
     return UserSession;
 }
