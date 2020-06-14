06ce6d2858ed6e250e63153061eeaec8f3a5a297
nadineb <nadineb@umich.edu>
2020-06-14 15:39:55 -0700

    [add] Router

---
+++ client/src/Components/Router/index.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import '../../App.css';
+
+import Home from "../../Pages/home";
+import NoMatch from "../../Pages/noMatch";
+import Profile from "../../Pages/profile";
+import Activities from "../../Pages/activities";
+import Login from "../../Pages/login";
+import Signup from "../../Pages/signup";
+
+import NavBar from "../NavBar"
+
+function Router() {
+
+    return (
+        <BrowserRouter>
+            <div>
+                <NavBar />
+                <Switch>
+                    <Route exact path="/" component={Home} />
+                    <Route exact path="/activities" component={Activities} />
+                    <Route exact path="/profile" component={Profile} />
+                    <Route exact path="/login" component={Login} />
+                    <Route exact path="/signup" component={Signup} />
+
+                    <Route component={NoMatch} />
+                </Switch>
+            </div>
+      </BrowserRouter>
+    )
+}
+
+export default Router;
