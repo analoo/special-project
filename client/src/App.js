90586ce369e9effb238d7b2190c4fea536ce4403
nadineb <nadineb@umich.edu>
2020-06-14 15:38:18 -0700

    [update] move router to seperate component

--- client/src/App.js
+++ client/src/App.js
@@ -1,37 +1,11 @@
 import React from 'react';
-import './App.css';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-
-import Home from "./Pages/home";
-import NoMatch from "./Pages/noMatch";
-import Profile from "./Pages/profile";
-import Activities from "./Pages/activities";
-import Login from "./Pages/login";
-import Signup from "./Pages/signup";
-
-
-
-import NavBar from "./Components/NavBar"
+import Router from "./Components/Router";
 
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <div>
-          <NavBar />
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/activities" component={Activities} />
-            <Route exact path="/profile" component={Profile} />
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/signup" component={Signup} />
-
-            <Route component={NoMatch} />
-          </Switch>
-        </div>
-      </Router>
-
+      <Router/>
     </div>
   );
 }
